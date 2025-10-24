8e6a08efdb6be351c8db37ff7d5e529895b04cd5
huidoudour <ylmf_qquser@126.com>
2025-10-24 20:01:20 +0800

    sdk29适配Material Design 3

--- app/src/main/kotlin/dev/sanmer/authenticator/ui/theme/Theme.kt
+++ app/src/main/kotlin/dev/sanmer/authenticator/ui/theme/Theme.kt
@@ -1,13 +1,16 @@
 package dev.sanmer.authenticator.ui.theme
 
+import android.os.Build
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.remember
@@ -35,9 +38,15 @@ fun AppTheme(
 @Composable
 private fun colorScheme(darkMode: Boolean): ColorScheme {
     val context = LocalContext.current
+    
+    // Dynamic color is available on Android 12+
+    val supportsDynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    
     return when {
-        darkMode -> dynamicDarkColorScheme(context)
-        else -> dynamicLightColorScheme(context)
+        supportsDynamicColor && darkMode -> dynamicDarkColorScheme(context)
+        supportsDynamicColor && !darkMode -> dynamicLightColorScheme(context)
+        darkMode -> darkColorScheme()
+        else -> lightColorScheme()
     }
 }
 
